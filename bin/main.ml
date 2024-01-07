e3ad6c186630a1adcf46651fd493c06044799163
midorishibukawa <midori@shibukawa.io>
2024-01-07 16:13:31 -0300

    add css

--- bin/main.ml
+++ bin/main.ml
@@ -1,13 +1,31 @@
+open Batteries
 module D = Dream
 module T = Templates
 
 let () = 
-    D.run ~tls:true
+    D.run 
     @@ D.logger 
     @@ D.memory_sessions
     @@ D.router [
 
         D.get "/static/**" @@ D.static "./static";
 
-        D.get "/" (fun _ -> D.html @@ T.page ~title:"hello, world" ~body:"hello, world");
+        D.get "/" (fun _ -> 
+            let open Dream_html in
+            let open HTML in
+            Dream_html.respond 
+            @@ T.page
+            ~title:"midori shibukawa"
+            @@ main [] 
+                [ h1 [] 
+                    [ strong [class_ "first"] [txt "%s" "MIDORI"]
+                    ; strong [class_ "last"] [txt "%s" "SHIBUKAWA"] ]
+                ; ul [] 
+                        @@ List.map 
+                        (fun i -> li [] [a [] [txt "%s" i]]) 
+                        ["about"; "blog"; "projects"; "contact"; "resume"]
+                ; section [id "background"] 
+                    @@ List.map (fun i -> span [id  "%s" ("bg-color-" ^ string_of_int i)] []) (List.of_enum (0--4))
+                ]
+            )
     ]
--- flake.nix
+++ flake.nix
@@ -5,51 +5,61 @@
     nixpkgs.follows = "opam-nix/nixpkgs";
   };
   outputs = { self, flake-utils, opam-nix, nixpkgs }@inputs:
-    flake-utils.lib.eachDefaultSystem (system:
+    # Don't forget to put the package name instead of `throw':
+    let package = "midori_shibukawa"; 
+    in flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
         on = opam-nix.lib.${system};
-        localPackagesQuery = builtins.mapAttrs (_: pkgs.lib.last)
-          (on.listRepo (on.makeOpamRepo ./.));
         devPackagesQuery = {
           # You can add "development" packages here. They will get added to the devShell automatically.
           ocaml-lsp-server = "*";
           ocamlformat = "*";
+          utop = "*";
         };
         query = devPackagesQuery // {
           ## You can force versions of certain packages here, e.g:
           ## - force the ocaml compiler to be taken from opam-repository:
           ocaml-base-compiler = "*";
+          ocaml-lsp-server = "*";
           ## - or force the compiler to be taken from nixpkgs and be a certain version:
           # ocaml-system = "4.14.0";
           ## - or force ocamlfind to be a certain version:
           # ocamlfind = "1.9.2";
         };
         scope = on.buildOpamProject' { } ./. query;
-        overlay = final: prev:
-          {
-            # You can add overrides here
-          };
+        prdScope = on.buildOpamProject' { pkgs = pkgs.pkgsStatic; } ./. query;
+        overlay = final: prev: {
+          # You can add overrides here
+          ${package} = prev.${package}.overrideAttrs (_: {
+            # Prevent the ocaml dependencies from leaking into dependent environments
+            doNixSupport = false;
+          });
+        };
         scope' = scope.overrideScope' overlay;
+        prdScope' = prdScope.overrideScope' overlay;
+        # The main package containing the executable
+        main = scope'.${package};
+        prd = prdScope'.${package};
         # Packages from devPackagesQuery
         devPackages = builtins.attrValues
           (pkgs.lib.getAttrs (builtins.attrNames devPackagesQuery) scope');
-        # Packages in this workspace
-        packages =
-          pkgs.lib.getAttrs (builtins.attrNames localPackagesQuery) scope';
+        prdPackages = builtins.attrValues
+          (pkgs.lib.getAttrs (builtins.attrNames devPackagesQuery) prdScope');
       in {
         legacyPackages = scope';
 
-        inherit packages;
-
-        ## If you want to have a "default" package which will be built with just `nix build`, do this instead of `inherit packages;`:
-        # packages = packages // { default = packages.<your default package>; };
+        packages = {
+            default = main;
+            prd = prd;
+        };
 
         devShells.default = pkgs.mkShell {
-          inputsFrom = builtins.attrValues packages;
+          inputsFrom = [ main ];
           buildInputs = devPackages ++ [
             # You can add packages from nixpkgs here
-          ];
+            
+          ]; 
         };
       });
 }
